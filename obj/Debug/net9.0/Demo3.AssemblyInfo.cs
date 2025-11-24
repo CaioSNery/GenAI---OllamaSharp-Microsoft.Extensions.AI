1c67ff50826a9b7ccb97afff4c900592eb3333a4
CaioSNery <caionery40@gmail.com>
2025-11-24 10:30:01 -0300

    first commit

--- obj/Debug/net9.0/Demo3.AssemblyInfo.cs
+++ obj/Debug/net9.0/Demo3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf44b23d44a18834aa3efec59fa7d6dc4dbae6c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880ddbe785de26cc193311691b06b62bd9957085")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
